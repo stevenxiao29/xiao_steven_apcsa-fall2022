8f21e53b3808d8514794b3863621f43f72a3efe3
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-08-18 11:14:51 -0700

    unit 2 upload

---
+++ Unit-02/src/Line.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( double x1, double y1, double x2, double y2 )
+	{
+		double m = (y2-y1)/(x2-x1);
+		return m;
+	}
+
+}
---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,15 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( double base1, double base2, double height )
+	{
+		double a = ((base1 + base2)/2)*height;
+		
+		return a;
+	}
+}
