1f67782a69e6ed156926157d98d78b43927a279d
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-08-18 10:47:46 -0700

    Add files via upload

---
+++ StarsAndStripes.java
@@ -0,0 +1,51 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public void pStarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   System.out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   System.out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println();
+	   System.out.println();
+
+   }
+   
+   public void printASmallBox()
+   {
+	   for (int i = 0; i < 5; i++) {
+		   printTwentyStars();
+		   printTwentyDashes();
+	   }
+   }
+ 
+   public void printABigBox()
+   { 	
+	   for (int i = 0; i < 10; i++) {
+		   printTwentyStars();
+		   printTwentyDashes();
+	   }
+   }   
+}
---
+++ StarsRunner.java
@@ -0,0 +1,23 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes test = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      test.pStarsAndStripes();
+      test.printASmallBox();
+      test.printTwoBlankLines();
+      test.printABigBox();
+   }
+}
