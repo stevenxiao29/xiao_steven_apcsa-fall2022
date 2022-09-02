231d8596eda09fca124b2f10d2ea79ddf59d37a7
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-09-02 11:40:55 -0700

    rps

--- Unit-07/src/RPSRunner.java
+++ Unit-07/src/RPSRunner.java
@@ -10,18 +10,20 @@ public class RPSRunner
 	public static void main(String args[])
 	{
 		Scanner keyboard = new Scanner(System.in);
-		char response;
+		String response;
 		
 		//add in a do while loop after you get the basics up and running
 		
 			String player = "";
 		
 			out.print("type in your prompt [R,P,S] :: ");
-			
+			response = keyboard.next();
 				//read in the player value
 		//use while loop response a == response b 
 			
-			RockPaperScissors game = new RockPaperScissors();		
+			RockPaperScissors game = new RockPaperScissors(response);
+			System.out.println(game);
+			
 	}
 }
 
--- Unit-07/src/RockPaperScissors.java
+++ Unit-07/src/RockPaperScissors.java
@@ -8,29 +8,64 @@ import static java.lang.System.*;
 public class RockPaperScissors
 {
 	private String playChoice;
-	private String compChoice;
+	private int compChoice;
+	private String cChoiceText = "";
+
 
 	public RockPaperScissors()
 	{
+		setPlayers(" ");
+		compChoice = (int) (Math.random()*3);
+		
+		if (compChoice == 0) {
+			cChoiceText = "R";
+		}else if (compChoice == 1) {
+			cChoiceText = "P";
+		}else if (compChoice == 0) {
+			cChoiceText = "S";
+		}
 	}
 
 	public RockPaperScissors(String player)
 	{
+		setPlayers(player);
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice = player;
 	}
 
 	public String determineWinner()
 	{
 		String winner="";
+		
+
+//		0=R
+//		1=P
+//		2=S
+		
+		if ((playChoice == "R" && compChoice == 1)||(playChoice == "S" && compChoice == 0)||(playChoice == "P" && compChoice == 2)) {
+			winner = "win";
+		}else {
+			
+		}
+		
+
 		return winner;
 	}
 
 	public String toString()
 	{
-		String output="";
+		String output= "";
+
+		if (determineWinner().equals("win")) {
+			output= "You win!!! \n" + playChoice + " beats " + cChoiceText ;
+		}
+		else {
+			output = "you lost unfortunately " + cChoiceText + " beats " + playChoice;
+			
+		}
 		return output;
 	}
 }
