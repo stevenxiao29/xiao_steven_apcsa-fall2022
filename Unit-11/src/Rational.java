3fed292be20c72963b19cb34f424dd1d70c30f71
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-10 00:13:11 -0700

    Update Rational.java

--- Unit-11/src/Rational.java
+++ Unit-11/src/Rational.java
@@ -1,77 +1,77 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
 
-import static java.lang.System.*; 
-
-class Rational implements Comparable<Rational>
+public class ToyStore
 {
-	//add two instance variables
+	private ArrayList<Toy> toyList;
 
-	//write two constructors
-
-
-	//write a setRational method
-	
-
-	//write  a set method for numerator and denominator
-
-	
-	public void add(Rational  other)
+	public ToyStore()
 	{
-		//num1/den1 + num2/den2 
-		//new numerator = (num1 * den2 + num2 * den1)
-		//new denominator = (den1 * den2)
+		toyList = new ArrayList<Toy>() ; 
+	}
+
+	public void loadToys( String toys )
+	{
+        String[] strSplit = toys.split(" "); 
+        
+  		for (int i = 0 ; i<strSplit.length; i++)
+  		{
+  			
+  			Toy t = new Toy(strSplit[i]);
+  			
+  			//if existing toyList does not have t in it then add
+  			//other, set new toy count ++
+  			
+  			toyList.add(t);
+  		}
+
+	}
+  
+  	public Toy getThatToy( String nm )
+  	{
+  		Toy t = new Toy(nm);
+  		return t;
+  	}
+  
+  	public Toy getMostFrequentToy()
+  	{
+  		int count = 0;
+  		int newCount = 0;
+  		int finalIndex = -1;
+  		
+  		for (int i = 0 ; i<toyList.size(); i++)
+  		{
+  			for (int j = 0 ; j<toyList.size(); j++) {
+  				if (toyList.get(i)==toyList.get(j))
+  					newCount++;
+  			}
+  			if (newCount > count) {
+  				finalIndex = i;
+  				count = newCount;
+  			}
+  			
+  		}
+  		
+  		return toyList.get(finalIndex);
+  		
+  	}  
+  
+  	public void sortToysByCount()
+  	{
+  	}  
+  	  
+	public String toString()
+	{
+		String out = "";
 		
+		for (int i = 0; i<toyList.size(); i++)
+		{
+			out += toyList.get(i).getName() + " " + toyList.get(i).getCount() + ", ";
+		}
 		
-
-
-		reduce();
+	   return "[" + out + "]" + "\n" + "max = " + getMostFrequentToy();
 	}
-
-	private void reduce()
-	{
-
-
-
-	}
-
-	private int gcd(int numOne, int numTwo)
-	{
-
-
-		return 1;
-	}
-
-	public Object clone ()
-	{
-		return "";
-	}
-
-
-	//ACCESSORS
-
-	//write get methods for numerator and denominator
-	
-	
-	public boolean equals( Object obj)
-	{
-
-
-		return false;
-	}
-
-	public int compareTo(Rational other)
-	{
-
-
-		return -1;
-	}
-
-
-
-	
-	//write  toString() method
-	
-	
 }
