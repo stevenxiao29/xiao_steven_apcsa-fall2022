b96f08a1ac8e6f9c99b08f7c1e89e7b9e1586c0a
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-10 00:14:28 -0700

    Update ToyStore.java

--- Unit-11/src/ToyStore.java
+++ Unit-11/src/ToyStore.java
@@ -1,7 +1,3 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -14,20 +10,53 @@ public class ToyStore
 
 	public ToyStore()
 	{
+		toyList = new ArrayList<Toy>() ; 
 	}
 
 	public void loadToys( String toys )
 	{
+        String[] strSplit = toys.split(" "); 
+        
+  		for (int i = 0 ; i<strSplit.length; i++)
+  		{
+  			
+  			Toy t = new Toy(strSplit[i]);
+  			
+  			//if existing toyList does not have t in it then add
+  			//other, set new toy count ++
+  			
+  			toyList.add(t);
+  		}
+
 	}
   
   	public Toy getThatToy( String nm )
   	{
-  		return null;
+  		Toy t = new Toy(nm);
+  		return t;
   	}
   
-  	public String getMostFrequentToy()
+  	public Toy getMostFrequentToy()
   	{
-  		return "";
+  		int count = 0;
+  		int newCount = 0;
+  		int finalIndex = -1;
+  		
+  		for (int i = 0 ; i<toyList.size(); i++)
+  		{
+  			for (int j = 0 ; j<toyList.size(); j++) {
+  				if (toyList.get(i)==toyList.get(j))
+  					newCount++;
+  			}
+  			if (newCount > count) {
+  				finalIndex = i;
+  				count = newCount;
+  			}
+  			
+  		}
+  		
+  		return toyList.get(finalIndex);
+  		
   	}  
   
   	public void sortToysByCount()
@@ -36,6 +65,13 @@ public class ToyStore
   	  
 	public String toString()
 	{
-	   return "";
+		String out = "";
+		
+		for (int i = 0; i<toyList.size(); i++)
+		{
+			out += toyList.get(i).getName() + " " + toyList.get(i).getCount() + ", ";
+		}
+		
+	   return "[" + out + "]" + "\n" + "max = " + getMostFrequentToy();
 	}
 }
