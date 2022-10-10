3278919ffaecb9bafb39c44ef03294a8e04287b0
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-10 00:14:10 -0700

    Update ToyRunner.java

--- Unit-11/src/ToyRunner.java
+++ Unit-11/src/ToyRunner.java
@@ -1,12 +1,14 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import static java.lang.System.*;
 
 public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Toy sorry = new Toy("sorry");
+		System.out.println(sorry);
+		
+		Toy joe= new Toy("gi joe");
+		joe.setCount(5);
+		System.out.println(joe);
 	}
 }
