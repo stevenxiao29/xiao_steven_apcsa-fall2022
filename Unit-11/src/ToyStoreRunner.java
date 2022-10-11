59d0fe2838b89d55f00d5a52dffef3e164961bf5
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-10 21:38:59 -0700

    Update ToyStoreRunner.java

--- Unit-11/src/ToyStoreRunner.java
+++ Unit-11/src/ToyStoreRunner.java
@@ -7,8 +7,10 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-		ArrayList testList = new ArrayList<String>();
-		ToyStore test = new ArrayList<String>();
+		//ArrayList testList = new ArrayList<String>();
+		ToyStore yuqian = new ToyStore();
+		yuqian.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball");
+		System.out.println(yuqian);
 	}
 
 }
