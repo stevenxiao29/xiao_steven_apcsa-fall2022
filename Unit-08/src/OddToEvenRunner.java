93b802c4727ca886ebb4cd5eeea5a84cf670fd22
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-14 10:53:44 -0700

    update

--- Unit-08/src/OddToEvenRunner.java
+++ Unit-08/src/OddToEvenRunner.java
@@ -9,6 +9,9 @@ public class OddToEvenRunner
 	{
 		RayOddToEven s = new RayOddToEven();
 		
+		System.out.print("\\dog\\cat");
+
+		
 		System.out.println( s.go( new int[]{7,1,5,3,11,5,6,7,8,9,10,12345,11} ) );
 		System.out.println( s.go( new int[]{11,9,8,7,6,5,4,3,2,1,-99,7} ) );
 		System.out.println( s.go( new int[]{10,20,30,40,5,41,31,20,11,7} ) );
--- Unit-11/src/RationalRunner.java
+++ Unit-11/src/RationalRunner.java
@@ -9,7 +9,7 @@ public class RationalRunner
 	public static void main( String args[] )
 	{
 		/*uncomment once Rational class is built
-		 *
+		 */
 		Rational test = new Rational();
 		out.println("test = " + test);
 
