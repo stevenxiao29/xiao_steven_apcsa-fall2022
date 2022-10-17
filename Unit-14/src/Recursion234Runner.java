c2548a5850ed7869cb9e004957b53e84a48d9c87
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-17 11:36:00 -0700

    unit 14

---
+++ Unit-14/src/Recursion234Runner.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class Recursion234Runner
+{
+	public static void main(String args[])
+	{
+	//	System.out.println(RecursionFunOne.countOddDigits(4532));
+		
+		
+		System.out.println(RecursionFunThree.luckyThrees(3323));
+		//add more test cases
+	
+
+
+	}
+}
