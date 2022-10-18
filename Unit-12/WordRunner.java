556fba42463c504e7db2742272792dc930d18bc9
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-17 23:32:16 -0700

    Update WordRunner.java

--- Unit-12/WordRunner.java
+++ Unit-12/WordRunner.java
@@ -5,6 +5,7 @@
 import java.io.File; 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 import static java.lang.System.*;
 
@@ -15,11 +16,45 @@ public class WordRunner
 		Scanner file = new Scanner(new File("words.dat"));
 
 		int size = file.nextInt();
-		file.nextLine();
 		
+		ArrayList <Word> wordList = new ArrayList();
+		file.nextLine();
+//		wordList.add(new Word()) ;
 
+		
+		
+		
+		for (int i = 0 ; i<= size ; i++)
+		{
+			System.out.println(i);
+			Word current = new Word(file.nextLine());
 
-
+			if (i ==0 )
+				wordList.add(current);
+			
+			else if (current.compareTo(wordList.get(wordList.size()-1))== 1) {
+				wordList.add(current);
+			}
+			else {
+				for (int j = wordList.size(); j > 0; j--) {
+					if (current.compareTo(wordList.get(j-1))== 1) {
+						wordList.add(current);
+						break;}
+					else if (current.compareTo(wordList.get(j-1))== -1) {
+						wordList.add(j-1,current);
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		//Word w = new Word(file.nextLine());
+		
+		
+		System.out.println(wordList);
+		
+		
 
 
 
