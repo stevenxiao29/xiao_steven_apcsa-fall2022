fde3797e368324df88d2c57ddaeb81859a9c097e
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-10-18 20:31:11 -0700

    Update WordRunner.java

--- Unit-12/WordRunner.java
+++ Unit-12/WordRunner.java
@@ -17,42 +17,25 @@ public class WordRunner
 
 		int size = file.nextInt();
 		
-		ArrayList <Word> wordList = new ArrayList();
+		Word[] wordList = new Word[size];
 		file.nextLine();
 //		wordList.add(new Word()) ;
-
 		
-		
-		
-		for (int i = 0 ; i<= size ; i++)
+		for (int i = 0 ; i< size ; i++)
 		{
-			System.out.println(i);
-			Word current = new Word(file.nextLine());
-
-			if (i ==0 )
-				wordList.add(current);
-			
-			else if (current.compareTo(wordList.get(wordList.size()-1))== 1) {
-				wordList.add(current);
-			}
-			else {
-				for (int j = wordList.size(); j > 0; j--) {
-					if (current.compareTo(wordList.get(j-1))== 1) {
-						wordList.add(current);
-						break;}
-					else if (current.compareTo(wordList.get(j-1))== -1) {
-						wordList.add(j-1,current);
-						break;
-					}
-				}
-			}
-			
+			wordList[i] = new Word(file.nextLine());
 		}
 		
+		Arrays.sort(wordList);
 		//Word w = new Word(file.nextLine());
 		
 		
-		System.out.println(wordList);
+		for (int i = 0 ; i< size ; i++)
+		{
+			System.out.println(wordList[i]);	
+		}
+		
+	
 		
 		
 
