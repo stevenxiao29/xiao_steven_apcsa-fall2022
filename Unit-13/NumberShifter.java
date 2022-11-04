d1408601a9c27615c88201ea3fa7c03420b4a9a5
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-11-03 20:20:01 -0700

    Update NumberShifter.java

--- Unit-13/NumberShifter.java
+++ Unit-13/NumberShifter.java
@@ -10,11 +10,61 @@ import static java.lang.System.*;
 
 public class NumberShifter
 {
-	public static int[] makeLucky7Array( int size)
+	static int [] finalArray; 
+	 static int size = 0;
+	
+	public NumberShifter(int a)
 	{
-		return null;
+		size = a;
+		finalArray = new int[a];
+		makeLucky7Array();
+	}
+	
+	public static void makeLucky7Array()
+	{
+		
+		for (int i = 0; i<size ; i++)
+		{
+			finalArray[i] = (int)(Math.random()*10 + 1);
+		}
+		
+		
 	}
 	public static void shiftEm(int[] array)
 	{
+		int tempIndex = 0;
+		
+		for (int i = 0; i<finalArray.length; i++)
+		{
+			if (finalArray[i]==7)
+			{
+				finalArray[i] = finalArray[tempIndex];
+				finalArray[tempIndex] = 7;
+				tempIndex++;
+			}
+		}
+	}
+	
+	public String toString()
+	{
+		
+		String out = "[";
+		
+		for (int i = 0; i<size; i++)
+		{
+			out += finalArray[i] + ", ";
+		}
+		
+		out += "]\n[";
+	
+		shiftEm(finalArray);
+
+		
+		for (int i = 0; i<size; i++)
+		{
+			out += finalArray[i] + ", ";
+		}
+		
+		return out +"]";
 	}
 }
