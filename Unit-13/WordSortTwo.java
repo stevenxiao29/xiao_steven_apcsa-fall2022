31cca0baa3f8cf82b1ba3ac2da6b39b06afda496
stevenxiao29 <111383004+stevenxiao29@users.noreply.github.com>
2022-11-03 20:20:38 -0700

    Update WordSortTwo.java

--- Unit-13/WordSortTwo.java
+++ Unit-13/WordSortTwo.java
@@ -12,15 +12,33 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		//sentence.useDelimiter(" ");
 	}
 
 	public void sort()
 	{
+		for (int i=1; i< wordRay.length; i++)
+		   {
+		      String word = wordRay[i];
+		      int j=i;
+		      while(j>0&&word.compareTo(wordRay[j-1])==-1){         
+		    	  wordRay[j]=wordRay[j-1];
+		         j--;
+		      }
+		      wordRay[j]=word;
+		   }
+
 	}
 
 	public String toString()
 	{
 		String output="";
+		
+		for (int i = 0; i<wordRay.length; i++)
+		{
+			output += wordRay[i] + "\n";
+		}
+		
 		return output+"\n\n";
 	}
 }
